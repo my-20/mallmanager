a87cb2c012be72f168113fe688a383ff9f447343
刘宇铖 <liuyucheng@mail.com>
2021-01-12 16:54:13 +0800

    实现用户列表渲染数据和用户状态开关

--- src/components/users/users.vue
+++ src/components/users/users.vue
@@ -18,23 +18,51 @@
 		</el-row>
 
 		<!-- 表格 -->
-		<el-table :data="tableData" style="width: 100%">
+		<el-table :data="userlist" style="width: 100%">
 			<el-table-column type="index" label="#" width="60">
 			</el-table-column>
-			<el-table-column prop="name" label="姓名" width="100">
+			<el-table-column prop="username" label="姓名" width="100">
 			</el-table-column>
 			<el-table-column prop="email" label="邮箱">
 			</el-table-column>
 			</el-table-column>
-			<el-table-column prop="email" label="电话">
+			<el-table-column prop="mobile" label="电话">
 			</el-table-column>
 			</el-table-column>
-			<el-table-column prop="email" label="创建时间">
+
+
+
+			<el-table-column prop="create_time" label="创建时间">
+				<!--如果单元格内显示的内容不是字符串(文本)，需要给被显示的内容外层包裹一个template -->
+				<!-- tempLate内部要用数据设置sLot-scope属性,该属性的值是要用数据create_time的数据源userList-->
+				<!-- slot-scope的值userlist其实就是el-table绑定的数据userlistuserlist.row->数组中的每个对象 -->
+				<!-- sLot-scope 作用:传值
+					sLot-scope的值会自动去上一级找最外层标签el-table所绑定的数据userlistsLot-scope="scope”此时"scope "==userlist数组
+					scope.row是数组的每个对象
+					scope.row.create_time我们要用的数据-->
+				<template slot-scope='userlist'>
+					{{userlist.row.create_time | fmtdate}}
+				</template>
 			</el-table-column>
+
+
+
 			</el-table-column>
-			<el-table-column prop="email" label="用户状态">
+			<el-table-column prop="mg_state" label="用户状态">
+				<!-- 由于el-table-column和template是不同的组件数据的作用域不同需要用在 template中需要用slot-scope来接收el-table-column的数据-->
+				<template slot-scope='scope'>
+					<!-- 用scope.row使用接收的数据 -->
+					<el-switch
+					  v-model="scope.row.mg_state"
+					  active-color="#13ce66"
+					  inactive-color="#ff4949">
+					  
+					</el-switch>
+				</template>
 			</el-table-column>
-			<el-table-column prop="email" label="操作">
+			
+			
+			<el-table-column prop="" label="操作">
 			</el-table-column>
 		</el-table>
 	</el-card>
@@ -45,24 +73,18 @@
 		data() {
 			return {
 				query: '',
-				pagenum:1,
-				pagesize:2,
-				tableData: [{
-					date: '2016-05-02',
-					name: '王小虎',
-					address: '上海市普陀区金沙江路 1518 弄'
-				}, {
-					date: '2016-05-04',
-					name: '王小虎',
-					address: '上海市普陀区金沙江路 1517 弄'
-				}, {
-					date: '2016-05-01',
-					name: '王小虎',
-					address: '上海市普陀区金沙江路 1519 弄'
-				}, {
-					date: '2016-05-03',
-					name: '王小虎',
-					address: '上海市普陀区金沙江路 1516 弄'
+				pagenum: 1,
+				pagesize: 2,
+				total: -1,
+				userlist: [{
+					id: 25,
+					username: "tige117",
+					mobile: "18616358651",
+					type: 1,
+					email: "tige112@163.com",
+					create_time: "2017-11-09T20:36:26.000Z",
+					mg_state: true, // 当前用户的状态
+					role_name: "炒鸡管理员"
 				}]
 			}
 		},
@@ -74,8 +96,26 @@
 				//请求数据需要授权的API，必须在请求头中使用Authorization 字段提供 token令牌
 				const AUTH_TOKEN = localStorage.getItem('token')
 				this.$http.defaults.headers.common['Authorization'] = AUTH_TOKEN
-				const res = await this.$http.get('users?query=${this.query}&pagenum=1&pagesize=2')
+				const res = await this.$http.get('users?query=' + this.query + '&pagenum=' + this.pagenum + '&pagesize=' + this.pagesize)
 				console.log(res)
+				const {
+					meta: {
+						status,
+						msg
+					},
+					data: {
+						users,
+						total
+					}
+				} = res.data
+				if (status === 200) {
+					this.userlist = JSON.parse(JSON.stringify(users))
+					this.total = total
+					this.$message.success(msg)
+
+				} else {
+					this.$message.warning(msg)
+				}
 			}
 		}
 	}
--- src/main.js
+++ src/main.js
@@ -7,10 +7,17 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import '@/assets/css/reset.css'
 import MyServerHttp from './plugins/http.js'
+import moment from 'moment'
+
 
 Vue.use(ElementUI);
 Vue.use(MyServerHttp);
 Vue.config.productionTip = false
+//全局过滤器-处理日期
+Vue.filter('fmtdate',(v)=>{
+	return moment(v).format('YYYY-MM-DD')
+})
+
 
 /* eslint-disable no-new */
 new Vue({
