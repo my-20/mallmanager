c65f937db152da40ddbb6584dfa0c407acc373ed
刘宇铖 <liuyucheng@mail.com>
2021-01-20 17:52:55 +0800

    商品管理 - 添加商品 - tabs标签

--- src/components/goods/goodsadd.vue
+++ src/components/goods/goodsadd.vue
@@ -10,7 +10,7 @@
 		<el-alert title="添加商品信息" type="success" center="" show-icon></el-alert>
 
 		<!-- 步骤条 el-steps-->
-		<el-steps :active="1" finish-status="success" simple style="margin-top: 20px">
+		<el-steps :active="1*active" finish-status="success" simple style="margin-top: 20px">
 			<el-step title="基本信息"></el-step>
 			<el-step title="商品参数"></el-step>
 			<el-step title="商品属性"></el-step>
@@ -18,10 +18,25 @@
 			<el-step title="商品内容"></el-step>
 		</el-steps>
 
+		<el-tabs v-model="active" tab-position="left" style="height:200px; ">
+			<el-tab-pane label="基本信息" name="1">基本信息</el-tab-pane>
+			<el-tab-pane label="商品参数" name="2">商品参数</el-tab-pane>
+			<el-tab-pane label="商品属性" name="3">商品属性</el-tab-pane>
+			<el-tab-pane label="商品图片" name="4">商品图片</el-tab-pane>
+			<el-tab-pane label="商品内容" name="5">商品内容</el-tab-pane>
+		</el-tabs>
+
 	</el-card>
 </template>
 
 <script>
+	export default {
+		data(){
+			return{
+				active:"1"
+			}
+		}
+	}
 </script>
 
 <style>
