dc0bf6cb86121357aee6271fb10613650873c700
刘宇铖 <liuyucheng@mail.com>
2021-01-20 18:26:17 +0800

    商品管理 - 添加商品 - 基本信息-表单绑定数据

--- src/components/goods/goodsadd.vue
+++ src/components/goods/goodsadd.vue
@@ -18,22 +18,49 @@
 			<el-step title="商品内容"></el-step>
 		</el-steps>
 
-		<el-tabs v-model="active" tab-position="left" style="height:200px; ">
-			<el-tab-pane label="基本信息" name="1">基本信息</el-tab-pane>
-			<el-tab-pane label="商品参数" name="2">商品参数</el-tab-pane>
-			<el-tab-pane label="商品属性" name="3">商品属性</el-tab-pane>
-			<el-tab-pane label="商品图片" name="4">商品图片</el-tab-pane>
-			<el-tab-pane label="商品内容" name="5">商品内容</el-tab-pane>
-		</el-tabs>
-
+		<el-form label-position="top" label-width="80px" :model="form" style="height:300px;">
+			<el-scrollbar style="height:100%">
+			<el-tabs v-model="active" tab-position="left" >
+				<el-tab-pane name="1" label="基本信息">
+					<el-form-item label="商品名称">
+						<el-input v-model="form.goods_name"></el-input>
+					</el-form-item>
+					<el-form-item label="商品价格">
+						<el-input v-model="form.goods_price"></el-input>
+					</el-form-item>
+					<el-form-item label="商品重量">
+						<el-input v-model="form.goods_weight"></el-input>
+					</el-form-item>
+					<el-form-item label="商品数量">
+						<el-input v-model="form.goods_number"></el-input>
+					</el-form-item>
+				</el-tab-pane>
+				<el-tab-pane name="2" label="商品参数"></el-tab-pane>
+				<el-tab-pane name="3" label="商品属性"></el-tab-pane>
+				<el-tab-pane name="4" label="商品图片"></el-tab-pane>
+				<el-tab-pane name="5" label="商品内容"></el-tab-pane>
+			</el-tabs>
+			</el-scrollbar>
+		</el-form>
 	</el-card>
 </template>
 
 <script>
 	export default {
-		data(){
-			return{
-				active:"1"
+		data() {
+			return {
+				active: "1",
+				form: {
+					gods_name:'',
+					goods_cat:'',
+					goods_price:'',
+					goods_numDer:'',
+					goods_weight: '',
+					goods_introduce: '',
+					pics:'',
+					attrs:''
+				}
+
 			}
 		}
 	}
