4db2b77015701d35e8ad56113443ea7089955983
刘宇铖 <liuyucheng@mail.com>
2021-01-21 16:48:21 +0800

    商品管理 - 添加商品 - 商品属性 - 商品参数 - 布局

--- src/components/goods/goodsadd.vue
+++ src/components/goods/goodsadd.vue
@@ -20,6 +20,7 @@
 		<!-- 外层需要表单来传数据发布发起请求 -->
 		<el-form label-position="top" label-width="80px" :model="form" style="height:300px;">
 			<el-scrollbar style="height:100%">
+				
 				<el-tabs @tab-click="tabChange()" v-model="active" tab-position="left">
 					<el-tab-pane name="1" label="基本信息">
 						<el-form-item label="商品名称">
@@ -37,8 +38,9 @@
 						<!-- 级联选择器 -->
 						<el-cascader clearable expand-trigger="hover" :options="options" v-model=" selectedOptions" :props="defaultProp"
 						 @change="handleChange"></el-cascader>
-
 					</el-tab-pane>
+					
+					
 					<el-tab-pane name="2" label="商品参数">
 						<!-- 显示的是该三级分类的商品参数 -->
 						<el-form-item :label="item1.attr_name" v-for="(item1,i) in arrDyparams" :key="i">
@@ -47,11 +49,14 @@
 								<el-checkbox border v-for="(item2,i) in item1.attr_vals" :key="i" :label="item2"></el-checkbox>
 							</el-checkbox-group>
 						</el-form-item>
-
 					</el-tab-pane>
 
-
-					<el-tab-pane name="3" label="商品属性"></el-tab-pane>
+					<el-tab-pane name="3" label="商品属性">
+						<!-- 静态参数的数据 -->
+						<el-form-item :label="item.attr_name" v-for="(item,i) in arrStaticparams" :key="i">
+							<el-input v-model="item.attr_vals"></el-input>
+						</el-form-item>
+					</el-tab-pane>
 					<el-tab-pane name="4" label="商品图片"></el-tab-pane>
 					<el-tab-pane name="5" label="商品内容"></el-tab-pane>
 				</el-tabs>
