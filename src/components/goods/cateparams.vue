3aa9a4b5b215e683c1276d6998fdf10885a11882
刘宇铖 <liuyucheng@mail.com>
2021-01-22 18:03:37 +0800

    商品管理 - 分类参数 - 动态参数 - 动态编辑 - 实现点击tag标签删除

--- src/components/goods/cateparams.vue
+++ src/components/goods/cateparams.vue
@@ -15,16 +15,18 @@
 			<el-tabs v-model="active" type="card" @tab-click="handleClick">
 				<el-tab-pane label="动态参数" name="1">
 					<el-button type="danger" plain>设置动态参数</el-button>
+
 					<!-- 展开行 -->
 					<el-table :data="arrDyparams" style="width: 100%">
 						<el-table-column type="expand" label="#">
 							<template slot-scope="scope">
+
 								<!-- el-tag -->
-								<el-tag :key="tag" v-for="tag in dynamicTags" closable :disable-transitions="false" @close="handleClose(tag)">
+								<el-tag :key="tag" v-for="tag in scope.row.attr_vals" closable :disable-transitions="false" @close="handleClose(scope.row,tag)">
 									{{tag}}
 								</el-tag>
 								<el-input class="input-new-tag" v-if="inputVisible" v-model="inputValue" ref="saveTagInput" size="small"
-								 @keyup.enter.native="handleInputConfirm" @blur="handleInputConfirm">
+								 @keyup.enter.native="handleInputConfirm(scope.row.attr_vals)" @blur="handleInputConfirm(scope.row.attr_vals)">
 								</el-input>
 								<el-button v-else class="button-new-tag" size="small" @click="showInput">+ New Tag</el-button>
 							</template>
@@ -105,13 +107,14 @@
 						item.attr_vals = item.attr_vals.length === 0 ? [] : item.attr_vals.trim().split(',')
 					});
 					//console.log(this.selectedOptions);
+					
 				} else {
 					if (this.selectedOptions.length !== 3) {
 						//提示
 						this.$message.error('请先选择商品分类中的三级分类');
 						//刷新视图
 						this.selectedOptions = [1, 3, 99999];
-						const res = await this.$http.get('categories/' + this.selectedOptions[2] + '/attributes?sel=many');
+						const res = await this.$http.get('categories/'+this.selectedOptions[2]+'/attributes?sel=many');
 						this.arrDyparams = res.data.data;
 						this.arrDyparams.forEach(item => {
 							//并不是所有的三级分类都有动态参数->""->[]->v-for报错
@@ -129,21 +132,29 @@
 			},
 			// tag方法
 			//tag 点击x的时候
-			hendleClose(tag) {
-				this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1);
+			async handleClose(row, tag) {
+				row.attr_vals.splice(row.attr_vals.indexOf(tag), 1);
+				var attr_name,attr_sel,attr_vals;
+				let putData = {
+					 attr_name:row.attr_name,
+					 attr_sel:'many',
+					 attr_vals:row.attr_vals.join(',')
+				}
+				const res = await this.$http.put("categories/"+this.selectedOptions[2]+"/attributes/"+row.attr_id,putData);
+				console.log(res);
 			},
 			//tag 点击newTag+按钮
 			showInput() {
-				this.inputvisible = true;
+				this.inputVisible = true;
 				this.$nextTick(_ => {
 					this.$refs.saveTagInput.$refs.input.focus();
 				});
 			},
 			//tag 回车键 or失去焦点
-			handleInputConfirm() {
+			handleInputConfirm(attr_vals) {
 				let inputValue = this.inputValue;
 				if (inputValue) {
-					this.dynamicTags.push(inputValue);
+					attr_vals.push(inputValue);
 				}
 				this.inputVisible = false;
 				this.inputValue = "";
@@ -153,19 +164,21 @@
 </script>
 
 <style>
-	.el-tag + .el-tag {
-	    margin-left: 10px;
-	  }
-	  .button-new-tag {
-	    margin-left: 10px;
-	    height: 32px;
-	    line-height: 30px;
-	    padding-top: 0;
-	    padding-bottom: 0;
-	  }
-	  .input-new-tag {
-	    width: 90px;
-	    margin-left: 10px;
-	    vertical-align: bottom;
-	  }
+	.el-tag+.el-tag {
+		margin-left: 10px;
+	}
+
+	.button-new-tag {
+		margin-left: 10px;
+		height: 32px;
+		line-height: 30px;
+		padding-top: 0;
+		padding-bottom: 0;
+	}
+
+	.input-new-tag {
+		width: 90px;
+		margin-left: 10px;
+		vertical-align: bottom;
+	}
 </style>
