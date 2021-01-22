7fe3389277173688ebce74b41281e47d0b4e062c
刘宇铖 <liuyucheng@mail.com>
2021-01-22 16:16:21 +0800

    商品管理 - 分类参数 - 动态参数 - 动态编辑 - tag配置

--- src/components/goods/cateparams.vue
+++ src/components/goods/cateparams.vue
@@ -11,30 +11,37 @@
 			</el-form-item>
 
 			<!-- tab -->
-			
-				<el-tabs v-model="active" type="card" @tab-click="handleClick">
-					<el-tab-pane label="动态参数" name="1">
-						<el-button type="danger" plain>设置动态参数</el-button>
-						<!-- 展开行 -->
-						<el-table :data="arrDyparams" style="width: 100%">
-							<el-table-column type="expand" label="#">
-								<template slot-scope="scope">
 
-								</template>
-							</el-table-column>
-							<el-table-column label="属性名称" prop="attr_name">
-							</el-table-column>
-							<el-table-column label="操作" prop="desc">
-								<el-button size="mini" plain type="primary" icon="el-icon-edit" circle @click='showEditUserDia(scope.row)'></el-button>
-								<el-button size="mini" plain type="danger" icon="el-icon-delete" circle @click="showDeleUserMsgBox(scope.row.id)"></el-button>
-							</el-table-column>
-						</el-table>
-					</el-tab-pane>
+			<el-tabs v-model="active" type="card" @tab-click="handleClick">
+				<el-tab-pane label="动态参数" name="1">
+					<el-button type="danger" plain>设置动态参数</el-button>
+					<!-- 展开行 -->
+					<el-table :data="arrDyparams" style="width: 100%">
+						<el-table-column type="expand" label="#">
+							<template slot-scope="scope">
+								<!-- el-tag -->
+								<el-tag :key="tag" v-for="tag in dynamicTags" closable :disable-transitions="false" @close="handleClose(tag)">
+									{{tag}}
+								</el-tag>
+								<el-input class="input-new-tag" v-if="inputVisible" v-model="inputValue" ref="saveTagInput" size="small"
+								 @keyup.enter.native="handleInputConfirm" @blur="handleInputConfirm">
+								</el-input>
+								<el-button v-else class="button-new-tag" size="small" @click="showInput">+ New Tag</el-button>
+							</template>
+						</el-table-column>
+						<el-table-column label="属性名称" prop="attr_name">
+						</el-table-column>
+						<el-table-column label="操作" prop="desc">
+							<el-button size="mini" plain type="primary" icon="el-icon-edit" circle @click='showEditUserDia(scope.row)'></el-button>
+							<el-button size="mini" plain type="danger" icon="el-icon-delete" circle @click="showDeleUserMsgBox(scope.row.id)"></el-button>
+						</el-table-column>
+					</el-table>
+				</el-tab-pane>
 
 
-					<el-tab-pane label="静态参数" name="2">静态参数</el-tab-pane>
-				</el-tabs>
-			
+				<el-tab-pane label="静态参数" name="2">静态参数</el-tab-pane>
+			</el-tabs>
+
 		</el-form>
 
 
@@ -59,7 +66,10 @@
 				//动态参数的数据数组
 				arrDyparams: [],
 				//静态参数的数据数组
-				arrStaticparams: []
+				arrStaticparams: [],
+				//tag数据
+				inputVisible: false,
+				inputValue: ''
 			}
 		},
 		created() {
@@ -93,15 +103,14 @@
 						//}
 						//item.attr_vals里的值为字符串，需要转成对象在遍历，trim()去前后空格，split(',')转换成对象以“，”分割
 						item.attr_vals = item.attr_vals.length === 0 ? [] : item.attr_vals.trim().split(',')
-						
 					});
-					console.log(this.selectedOptions);
+					//console.log(this.selectedOptions);
 				} else {
 					if (this.selectedOptions.length !== 3) {
 						//提示
 						this.$message.error('请先选择商品分类中的三级分类');
 						//刷新视图
-						this.selectedOptions=[1,3,99999];
+						this.selectedOptions = [1, 3, 99999];
 						const res = await this.$http.get('categories/' + this.selectedOptions[2] + '/attributes?sel=many');
 						this.arrDyparams = res.data.data;
 						this.arrDyparams.forEach(item => {
@@ -111,16 +120,52 @@
 							//}
 							//item.attr_vals里的值为字符串，需要转成对象在遍历，trim()去前后空格，split(',')转换成对象以“，”分割
 							item.attr_vals = item.attr_vals.length === 0 ? [] : item.attr_vals.trim().split(',')
-							
+
 						});
-						console.log(this.selectedOptions);
+						//console.log(this.selectedOptions);
 						return;
 					}
 				}
+			},
+			// tag方法
+			//tag 点击x的时候
+			hendleClose(tag) {
+				this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1);
+			},
+			//tag 点击newTag+按钮
+			showInput() {
+				this.inputvisible = true;
+				this.$nextTick(_ => {
+					this.$refs.saveTagInput.$refs.input.focus();
+				});
+			},
+			//tag 回车键 or失去焦点
+			handleInputConfirm() {
+				let inputValue = this.inputValue;
+				if (inputValue) {
+					this.dynamicTags.push(inputValue);
+				}
+				this.inputVisible = false;
+				this.inputValue = "";
 			}
 		}
 	}
 </script>
 
 <style>
+	.el-tag + .el-tag {
+	    margin-left: 10px;
+	  }
+	  .button-new-tag {
+	    margin-left: 10px;
+	    height: 32px;
+	    line-height: 30px;
+	    padding-top: 0;
+	    padding-bottom: 0;
+	  }
+	  .input-new-tag {
+	    width: 90px;
+	    margin-left: 10px;
+	    vertical-align: bottom;
+	  }
 </style>
