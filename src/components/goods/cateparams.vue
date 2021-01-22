3f378644fefa683a445457b8a8b6282b8bbbe57d
刘宇铖 <liuyucheng@mail.com>
2021-01-22 13:44:29 +0800

    商品管理 - 分类参数 - 动态参数 - 获取动态参数数据

--- src/components/goods/cateparams.vue
+++ src/components/goods/cateparams.vue
@@ -7,7 +7,8 @@
 		<el-form label-position="left" label-width="80px" style="height:400px;">
 			<!-- 级联选择器 -->
 			<el-form-item label="商品分类">
-				<el-cascader clearable expand-trigger="hover" :options="options" v-model=" selectedOptions" :props="defaultProp"></el-cascader>
+				<el-cascader clearable expand-trigger="hover" :options="options" v-model=" selectedOptions" :props="defaultProp"
+				 @change="handleChange()"></el-cascader>
 			</el-form-item>
 		</el-form>
 	</el-card>
@@ -26,7 +27,11 @@
 					label: 'cat_name',
 					value: 'cat_id',
 					children: 'children'
-				}
+				},
+				//动态参数的数据数组
+				arrDyparams: [],
+				//静态参数的数据数组
+				arrStaticparams: []
 			}
 		},
 		created() {
@@ -39,6 +44,33 @@
 				//console.log(res);
 				this.options = res.data.data;
 				//console.log(this.options);
+			},
+			// 级联选择器@change触发的方法
+			async handleChange() {
+				if(this.selectedOptions.length===3){
+					//获取数据
+					//id->分类 sel=many表示的是获取动态参数的数据
+					const res = await this.$http.get('categories/' + this.selectedOptions[2] + '/attributes?sel=many');
+					//console.log(res);
+					this.arrDyparams = res.data.data;
+					//console.log(this.arrDyparams);
+					//this.arrDyparams每个对象.attr_vals字符串->数组->v-for
+					this.arrDyparams.forEach(item => {
+						//并不是所有的三级分类都有动态参数->""->[]->v-for报错
+						// if (item.attr_vals.Length !==O) {
+						// item.attr_vaLs = item.attr_vals.trim( ).split( ',')
+						//}
+						//item.attr_vals里的值为字符串，需要转成对象在遍历，trim()去前后空格，split(',')转换成对象以“，”分割
+						item.attr_vals = item.attr_vals.length === 0 ? [] : item.attr_vals.trim().split(',')
+					});
+					console.log(this.arrDyparams);
+				}else{
+					if (this.selectedOptions.length !== 3) {
+						//提示
+						this.$message.error('请先选择商品分类中的三级分类');
+						return;
+					}
+				}
 			}
 		}
 	}
