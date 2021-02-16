0026017aad07a20bb06f6d3263e70b8d3c396d0e
my-20 <1632273577@qq.com>
2021-02-16 16:45:03 +0800

    侧边导航栏调整

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,60 +1,127 @@
 <template>
-    <el-container class="container">
-        <el-header class="header">
-            <el-row>
-                
-                <el-col :span="22" class="middle">
-                    <h2>电商管理系统</h2>
-                </el-col>
-                <el-col :span="2">
-                    
-                    <a href="#" class="loginout">退出</a>
-                </el-col>
-            </el-row>
-        </el-header>
-        <el-container>
-            <el-aside class="aside" width="200px">Aside</el-aside>
-            <el-main class="main">Main</el-main>
-        </el-container>
-    </el-container>
+<el-container class="container">
+    <el-header class="header">
+        <el-row>
 
+            <el-col :span="22" class="middle">
+                <h2>电商管理系统</h2>
+            </el-col>
+            <el-col :span="2">
+                <a href="#" class="loginout">退出</a>
+            </el-col>
+        </el-row>
+    </el-header>
+    <el-container>
+        <el-aside class="aside" width="200px">
+            <el-menu :unique-opened="true">
+                <!-- 导航一 -->
+                <el-submenu index="1">
+                    <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>用户管理</span>
+                    </template>
+                    <el-menu-item index="1-1">
+                        <i class="el-icon-menu"></i>
+                        <span>用户列表</span>
+                    </el-menu-item>
+                </el-submenu>
+                <!-- 导航二 -->
+                <el-submenu index="2">
+                    <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>权限管理</span>
+                    </template>
+                    <el-menu-item index="1-1">
+                        <i class="el-icon-menu"></i>
+                        <span>角色列表</span>
+                    </el-menu-item>
+                    <el-menu-item index="1-1">
+                        <i class="el-icon-menu"></i>
+                        <span>权限列表</span>
+                    </el-menu-item>
+                </el-submenu>
+                <!-- 导航三 -->
+                <el-submenu index="3">
+                    <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>商品管理</span>
+                    </template>
+                    <el-menu-item index="1-1">
+                        <i class="el-icon-menu"></i>
+                        <span>商品列表</span>
+                    </el-menu-item>
+                    <el-menu-item index="1-1">
+                        <i class="el-icon-menu"></i>
+                        <span>分类参数</span>
+                    </el-menu-item>
+                    <el-menu-item index="1-1">
+                        <i class="el-icon-menu"></i>
+                        <span>商品分类</span>
+                    </el-menu-item>
+                </el-submenu>
+                <!-- 导航四 -->
+                <el-submenu index="4">
+                    <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>订单管理</span>
+                    </template>
+                    <el-menu-item index="1-1">
+                        <i class="el-icon-menu"></i>
+                        <span>订单列表</span>
+                    </el-menu-item>
+                </el-submenu>
+                <!-- 导航五 -->
+                <el-submenu index="5">
+                    <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>数据统计</span>
+                    </template>
+                    <el-menu-item index="1-1">
+                        <i class="el-icon-menu"></i>
+                        <span>数据报表</span>
+                    </el-menu-item>
+                </el-submenu>
+            </el-menu>
+        </el-aside>
+        <el-main class="main">Main</el-main>
+    </el-container>
+</el-container>
 </template>
 
 <script>
 export default {
-    
 
 }
 </script>
 
 <style>
 /* 主页面整体 */
-.container{
+.container {
     height: 100%;
 }
 
 /* 主页面头部 */
-.header{
+.header {
     background-color: #b3c0d1;
 }
 
 /* 主页面侧边 */
-.aside{
+.aside {
     background-color: #d3dce6;
 }
 
 /* 主页面主体内容 */
-.main{
+.main {
     background-color: #e9eef3;
 }
 
 /* 主页面头部文字 */
-.middle{
+.middle {
     text-align: center;
 }
 
 /* 主页面头部退出标签 */
-.loginout{
+.loginout {
     line-height: 60px;
     text-decoration: none;
 }
--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,29 +1,29 @@
 <template>
-    <div class="login-wrap">
-        <el-form class="login-form" label-position="top" label-width="80px" :model="formdata">
-            <h2>用户登录</h2>
-            <el-form-item label="用户名">
-                <el-input v-model="formdata.username"></el-input>
-            </el-form-item>
-            <el-form-item label="密码">
-                <el-input v-model="formdata.password"></el-input>
-            </el-form-item>
-            <el-button @click.prevent = "handleLogin()" type="primary" class="login-btn">登录</el-button>          
-        </el-form>
-    </div>
+<div class="login-wrap">
+    <el-form class="login-form" label-position="top" label-width="80px" :model="formdata">
+        <h2>用户登录</h2>
+        <el-form-item label="用户名">
+            <el-input v-model="formdata.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+            <el-input v-model="formdata.password"></el-input>
+        </el-form-item>
+        <el-button @click.prevent="handleLogin()" type="primary" class="login-btn">登录</el-button>
+    </el-form>
+</div>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            formdata:{
-                username:'',
-                password:''
+    data() {
+        return {
+            formdata: {
+                username: '',
+                password: ''
             }
         }
     },
-    methods:{
+    methods: {
         //登录请求
         // handleLogin(){
         //     this.$http.post('login',this.formdata).then(res => {
@@ -46,30 +46,30 @@ export default {
 
         //使用ES7 async+await
         //希望让异步操作代码，看起来像同步代码
-        async handleLogin(){
-            const res = await this.$http.post('login',this.formdata)
-                const{
-                    data,
-                    meta:{msg,status}
-                } = res.data
+        async handleLogin() {
+            const res = await this.$http.post('login', this.formdata)
+            const {
+                data,
+                meta: { msg, status }
+            } = res.data
 
-                if(status === 200){
-                    //跳转home页面
-                    this.$router.push({name:'home'})
-                    //提示成功
-                    this.$message.success(msg)
-                }else{
-                    //提示失败
-                    this.$message.warning(msg)
-                }
-            
+            if (status === 200) {
+                //跳转home页面
+                this.$router.push({ name: 'home' })
+                //提示成功
+                this.$message.success(msg)
+            } else {
+                //提示失败
+                this.$message.warning(msg)
+            }
+
         }
     }
 }
 </script>
 
 <style>
-.login-wrap{
+.login-wrap {
     height: 100%;
     background-color: #324152;
     /* 弹性盒布局 */
@@ -78,14 +78,14 @@ export default {
     align-items: center;
 }
 
-.login-wrap .login-form{
+.login-wrap .login-form {
     width: 400px;
     background-color: #fff;
     border-radius: 5px;
     padding: 30px;
 }
 
-.login-wrap .login-btn{
+.login-wrap .login-btn {
     width: 100%;
 }
 </style>
